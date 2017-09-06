c572f61e1619e7d4fec9dfa9c6bd89fe0dbc7227
anhboy2002 <anhboy2002@gmail.com>
2017-09-06 22:29:55 +0700

    push code on github

---
+++ app/src/main/java/layoutfacebook/gamelofd/com/layoutfacebook/MainActivity.java
@@ -0,0 +1,13 @@
+package layoutfacebook.gamelofd.com.layoutfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
